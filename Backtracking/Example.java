bbe57363ce1caf2973e3a0427866f3613869207e
prabhakarzha <kumarprabhakar141@gmail.com>
2023-01-12 22:14:36 +0530

    backtracking problem

--- Backtracking/Example.java
+++ Backtracking/Example.java
@@ -12,6 +12,7 @@ public class Example {
         changeArr(arr,i+1,val+1);
         arr[i] = arr[i]-2;
     }
+    
 
     public static void printArr(int[] arr){
         for(int i=0; i<arr.length; i++){
